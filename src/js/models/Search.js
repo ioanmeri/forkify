7c75bc156416e703cdf2686cc043b3888575020b
im <im@example.com>
2019-01-26 01:40:57 +0200

    Implemented Search model

---
+++ src/js/models/Search.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import config from './config';
+
+export default class Search {
+	constructor(query){
+		this.query = query;
+	}
+
+	async getResults(){
+
+		try {
+			const res = await axios(`https://www.food2fork.com/api/search?key=${config.forkKey}&q=${this.query}`);
+			this.result = res.data.recipes;
+			console.log(this.result);
+		} catch (error){
+			alert(error);
+		}
+
+	}
+}
+
+
+
+
