b31da935b1e0870979a86a22565343ee4de21090
im <im@example.com>
2019-01-26 21:12:15 +0200

    Render an AJAX Loading Spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
-
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 /** Global state of the app 
   * - Search object
   * - Current recipe object
@@ -10,23 +11,28 @@ const state = {};
 
 const controlSearch = async () => {
 	// 1) Get query from the view
-	const query = 'pizza' // TODO1
+	const query = searchView.getInput();
 
 	if(query){
 		// 2) New search object and add to state
 		state.search = new Search(query);
 
 		// 3) Prepare UI for results
+		searchView.clearInput();
+		searchView.clearResults();
+		renderLoader(elements.searchRes);
+
 
 		// 4) Search for recipes
 		await state.search.getResults();
 
 		// 5) Render results on UI
-		console.log(state.search.result)
+		clearLoader();
+		searchView.renderResults(state.search.result);
 	}
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
 	e.preventDefault();
 	controlSearch();
 });
